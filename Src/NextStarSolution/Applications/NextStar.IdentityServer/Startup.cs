5a83b04c1c38f07a76a74142721412896b9fc95c
SpiritLing <spirit_ling_cn@163.com>
2021-10-26 13:10:08 +0900

    fix(Solution): 修改所有认证地址为http

--- Src/NextStarSolution/Applications/NextStar.IdentityServer/Startup.cs
+++ Src/NextStarSolution/Applications/NextStar.IdentityServer/Startup.cs
@@ -54,9 +54,14 @@ namespace NextStar.IdentityServer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseStatusCodePages();
 
-            app.UseHttpsRedirection();
+            if (env.IsProduction())
+            {
+                app.UseHttpsRedirection();
+            }
+            
             app.UseStaticFiles();
 
             app.UseRouting();
