e0d60e9f3002d791663510983c55fc27886358bc
SpiritLing <spirit_ling_cn@163.com>
2021-11-03 13:11:35 +0900

    fix(Solution): 移动swagger到具体的API层面

--- Src/NextStarSolution/Shared/NextStar.Framework.AspNetCore/Extensions/ServiceCollectionExtensions.cs
+++ Src/NextStarSolution/Shared/NextStar.Framework.AspNetCore/Extensions/ServiceCollectionExtensions.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using NextStar.Framework.Abstractions.Cache;
 using NextStar.Framework.Abstractions.Config;
 using NextStar.Framework.Abstractions.Session;
@@ -97,52 +96,6 @@ namespace NextStar.Framework.AspNetCore.Extensions
             return services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = appSetting.DataBaseSetting.Redis;
-            });
-        }
-
-        /// <summary>
-        /// API 添加自定义swagger
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="nameSpace"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddCustomSwaggerGen(this IServiceCollection services,string nameSpace)
-        {
-            return services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = $"NextStar.{nameSpace}.API", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header,
-                        },
-                        new List<string>()
-                    }
-                });
-                
-                // Set the comments path for the Swagger JSON and UI.
-                // 需要在 .csproj 文件中添加
-                // <PropertyGroup> <GenerateDocumentationFile>true</GenerateDocumentationFile> </PropertyGroup>
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
             });
         }
 
