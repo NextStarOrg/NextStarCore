d8401a80633016f0f9c3f1d9a43a1453db3416d3
SpiritLing <spirit_ling_cn@163.com>
2021-11-18 18:41:49 +0900

    fix(blogService): api接口修改

--- Src/NextStarSolution/MicroServices/NextStar.BlogService/NextStar.BlogService.Core/Entities/Author/AuthorSelectInput.cs
+++ Src/NextStarSolution/MicroServices/NextStar.BlogService/NextStar.BlogService.Core/Entities/Author/AuthorSelectInput.cs
@@ -6,7 +6,7 @@ namespace NextStar.BlogService.Core.Entities;
 public class AuthorSelectInput:ICommonInput
 {
     public string SearchText { get; set; }
-    public List<SortDescriptor> Sorts { get; set; }
-    public int PageSize { get; set; }
-    public int PageIndex { get; set; }
+    public List<SortDescriptor> Sorts { get; set; } = new List<SortDescriptor>();
+    public int PageSize { get; set; } = 10;
+    public int PageIndex { get; set; } = 1;
 }
