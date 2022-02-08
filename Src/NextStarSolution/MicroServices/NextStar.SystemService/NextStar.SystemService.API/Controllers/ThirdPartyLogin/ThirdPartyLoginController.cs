014fa016401b8ed30672c3dca0fefe5c2360639c
AlbertXiaoPeng <albert.xiao@grapecity.com>
2022-02-08 17:21:20 +0900

    feat(SystemService): 添加第三方登录接口

---
+++ Src/NextStarSolution/MicroServices/NextStar.SystemService/NextStar.SystemService.API/Controllers/ThirdPartyLogin/ThirdPartyLoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NextStar.Library.MicroService.Outputs;
+
+namespace NextStar.SystemService.API.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class ThirdPartyLoginController : ControllerBase
+{
+    private readonly ILogger<ThirdPartyLoginController> _logger;
+
+    public ThirdPartyLoginController(ILogger<ThirdPartyLoginController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ICommonDto<bool>> GetList()
+    {
+        return CommonDto<bool>.Ok(true);
+    }
+}
